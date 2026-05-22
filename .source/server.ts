98217f54ac2947c611e9dac60795c075d3f33f17
man-navlakha <mannavlakha1021@gmail.com>
2026-05-23 01:55:52 +0530

    Add Vehicle API and related documentation; update existing API titles and metadata

--- .source/server.ts
+++ .source/server.ts
@@ -1,11 +1,15 @@
 // @ts-nocheck
-import * as __fd_glob_7 from "../content/docs/introduction/pin-api.mdx?collection=docs"
-import * as __fd_glob_6 from "../content/docs/introduction/icons-api.mdx?collection=docs"
-import * as __fd_glob_5 from "../content/docs/introduction/github-stats.mdx?collection=docs"
-import * as __fd_glob_4 from "../content/docs/introduction/animal-api.mdx?collection=docs"
-import * as __fd_glob_3 from "../content/docs/index.mdx?collection=docs"
-import * as __fd_glob_2 from "../content/docs/api.mdx?collection=docs"
-import { default as __fd_glob_1 } from "../content/docs/introduction/meta.json?collection=docs"
+import * as __fd_glob_11 from "../content/docs/introduction/vehicle-api.mdx?collection=docs"
+import * as __fd_glob_10 from "../content/docs/introduction/pin-api.mdx?collection=docs"
+import * as __fd_glob_9 from "../content/docs/introduction/index.mdx?collection=docs"
+import * as __fd_glob_8 from "../content/docs/introduction/icons-api.mdx?collection=docs"
+import * as __fd_glob_7 from "../content/docs/introduction/github-stats.mdx?collection=docs"
+import * as __fd_glob_6 from "../content/docs/introduction/animal-api.mdx?collection=docs"
+import * as __fd_glob_5 from "../content/docs/advanced/index.mdx?collection=docs"
+import * as __fd_glob_4 from "../content/docs/index.mdx?collection=docs"
+import * as __fd_glob_3 from "../content/docs/api.mdx?collection=docs"
+import { default as __fd_glob_2 } from "../content/docs/introduction/meta.json?collection=docs"
+import { default as __fd_glob_1 } from "../content/docs/advanced/meta.json?collection=docs"
 import { default as __fd_glob_0 } from "../content/docs/meta.json?collection=docs"
 import { server } from 'fumadocs-mdx/runtime/server';
 import type * as Config from '../source.config';
@@ -15,4 +19,4 @@ const create = server<typeof Config, import("fumadocs-mdx/runtime/types").Intern
   }
 }>({"doc":{"passthroughs":["extractedReferences"]}});
 
-export const docs = await create.docs("docs", "content/docs", {"meta.json": __fd_glob_0, "introduction/meta.json": __fd_glob_1, }, {"api.mdx": __fd_glob_2, "index.mdx": __fd_glob_3, "introduction/animal-api.mdx": __fd_glob_4, "introduction/github-stats.mdx": __fd_glob_5, "introduction/icons-api.mdx": __fd_glob_6, "introduction/pin-api.mdx": __fd_glob_7, });
+export const docs = await create.docs("docs", "content/docs", {"meta.json": __fd_glob_0, "advanced/meta.json": __fd_glob_1, "introduction/meta.json": __fd_glob_2, }, {"api.mdx": __fd_glob_3, "index.mdx": __fd_glob_4, "advanced/index.mdx": __fd_glob_5, "introduction/animal-api.mdx": __fd_glob_6, "introduction/github-stats.mdx": __fd_glob_7, "introduction/icons-api.mdx": __fd_glob_8, "introduction/index.mdx": __fd_glob_9, "introduction/pin-api.mdx": __fd_glob_10, "introduction/vehicle-api.mdx": __fd_glob_11, });
